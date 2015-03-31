2432d826753e96f2527ee0e6386a422edefc8a1b
djherbis <djherbis@gmail.com>
2015-03-31 19:04:23 -0400

    adding godoc comments

--- socket.go
+++ socket.go
@@ -5,16 +5,31 @@ import (
 	"sync"
 )
 
+// Emitter handles sending messages
 type Emitter interface {
-	Emit(string, ...interface{}) error
+
+	// Emit sends an event and args which will be handled by the func
+	// registered via socket.on.
+	Emit(event string, args ...interface{}) error
 }
 
+// Socket is a Server-side socket
 type Socket interface {
 	ClientSocket
-	Join(string)
-	Leave(string)
+
+	// Join adds a Socket to the passed Room
+	Join(room string)
+
+	// Leave removes a Socket from the passed Room
+	Leave(room string)
+
+	// Rooms returns the rooms this Socket is in
 	Rooms() []string
+
+	// To returns an Emitter to the passed room.
 	To(string) Emitter
+
+	// Request returns the request which this Socket was created from.
 	Request() *http.Request
 }
 
